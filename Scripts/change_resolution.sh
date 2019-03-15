4c066762b12f8e49c6e3386b1934ed2866119873
lorenzomoulin <lorenzomoulin@gmail.com>
2019-03-15 15:04:40 -0300

    Update change_resolution.sh

--- Scripts/change_resolution.sh
+++ Scripts/change_resolution.sh
@@ -2,7 +2,7 @@
 
 #script to help people with issues on changing resolution of the monitor
 
-password="123456"
+password=""
 device="DVI-I-1-1"
 output="$(cvt 1920 1080 | grep Modeline | awk -F'Modeline ' '{print $2}')"
 echo "$password" | sudo -S xrandr --newmode $output
