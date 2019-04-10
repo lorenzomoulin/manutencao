88b12dcd6bfcf7fc35ddfa4eb388f667ee27af0e
lorenzomoulin <lorenzomoulin@gmail.com>
2019-04-10 15:42:16 -0300

    Update info.sh

--- info.sh
+++ info.sh
@@ -7,6 +7,8 @@ cat /proc/cpuinfo | grep "model name" >> info.txt
 
 echo INFORMACOES DA MEMORIA >> info.txt
 cat /proc/meminfo | grep "MemTotal" >> info.txt
+sudo dmidecode | grep -i "Maximum Capacity:" | uniq >> info.txt
+sudo lshw -class memory | awk '/bank/ {count++} END {print "You have " count " slots for RAM"}' >> info.txt
 
 echo INFORMACOES DA PLACA MAE >> info.txt
 sudo dmidecode | grep Product >> info.txt
